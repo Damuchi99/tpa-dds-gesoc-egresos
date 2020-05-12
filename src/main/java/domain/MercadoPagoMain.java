1fda9b1e3f6e7e5d025b5ae50eb0ce686dfe99f9
Damuchi99 <damuchi99@gmail.com>
2020-05-11 23:33:27 -0300

    Pruebo la API de MercadoPago

---
+++ src/main/java/domain/MercadoPagoMain.java
@@ -0,0 +1,29 @@
+package domain;
+
+import com.mercadopago.*;
+import com.mercadopago.exceptions.MPConfException;
+import com.mercadopago.exceptions.MPException;
+import com.mercadopago.resources.Payment;
+import com.mercadopago.resources.datastructures.payment.Payer;
+
+public abstract class MercadoPagoMain {
+	public static void main(String[] args)throws MPException, MPConfException {
+
+        MercadoPago.SDK.setClientSecret(System.getenv("CLIENT_SECRET_OK"));
+        MercadoPago.SDK.setClientId(System.getenv("CLIENT_ID_OK"));
+
+        Payment payment = new Payment()
+                .setTransactionAmount(100f)
+                .setToken("your_cardtoken")
+                .setDescription("description")
+                .setInstallments(1)
+                .setPaymentMethodId("visa")
+                .setPayer(new Payer()
+                        .setEmail("dummy_email"));
+
+        payment.save();
+
+        System.out.println(payment.getStatus());
+
+    }
+}
